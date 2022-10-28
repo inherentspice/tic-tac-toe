068749d9980dd57406c5086b88b2f5cb211c1aeb
Stephen Smith <stephenlehanesmith@gmail.com>
2022-10-28 10:52:31 +0800

    added ESLint to project

--- index.js
+++ index.js
@@ -1,58 +1,65 @@
-console.log("Connected...")
+console.log("Connected...");
 
-const gameBoard = (event) => {
+const gameBoard = () => {
   // here we want an array of places on the screen
   // give pieces an owner that way we track the winner
-  console.log("making game board...")
-
+  console.log("making game board...");
 };
 
 const endGame = () => {
   // checks if there are three tiles horizontally, vertically, or diagonally
+  console.log("ending game...");
 };
 
 const addPiece = () => {
   // updates ownership of tiles in gameBoard object
+  console.log("adding piece...");
 };
 
-const players = ((name) => {
+const players = (() => {
   // players need--name, symbol, and turn
-  this.add = function() {
-    console.log("adding player...")
-  }
+  const add = () => {
+    console.log("adding player...");
+  };
   return {
-    add
-  }
+    add,
+  };
 })();
 
 const domCache = () => {
-  this.startGameButton = document.getElementById("new-game");
-  this.gameBoardDisplay = document.getElementById("game-board");
+  const startGameButton = document.getElementById("new-game");
+  const gameBoardDisplay = document.getElementById("game-board");
 
   return {
     startGameButton,
-    gameBoardDisplay
+    gameBoardDisplay,
   };
-}
+};
 
 const gameFlow = (() => {
-  let playerOne = {};
-  let playerTwo = {};
+  const playerOne = {};
+  const playerTwo = {};
 
   const cached = domCache();
 
-  //bindEvents
+  // bindEvents
   cached.startGameButton.addEventListener("click", (e) => {
     e.preventDefault();
-    players.add('player one')
+    players.add("player one");
     gameBoard();
-  })
+  });
 
-  //render
+  console.log(playerOne);
+  console.log(playerTwo);
+  addPiece();
+  endGame();
 
+  // render
   // first get players
   // then display gameboard
   // bindevents
   // call endGame checker
   // clear board when game is over
 })();
+
+gameFlow();
